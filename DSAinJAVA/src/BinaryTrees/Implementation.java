b9e974e782444dc9517ec51fe41915bca608252f
RaushanDevloper <raushankumarsingh7033@gmail.com>
2026-06-15 17:32:05 +0530

    Add Code

--- DSAinJAVA/src/BinaryTrees/Implementation.java
+++ DSAinJAVA/src/BinaryTrees/Implementation.java
@@ -12,6 +12,11 @@ class Node {
 
 public class Implementation {
     public static void main(String[] args) {
+        //       3
+        //      /  \
+        //     4    2
+        //    / \  / \
+        //  -1   1 6  9
 
         // Direct root only
         Node a = new Node(3);
@@ -27,11 +32,23 @@ public class Implementation {
         c.left = f;c.right = g;
 
         // Sirf root print hoga
-        display(a);
+        preorder(a);
+        System.out.println();
+        inorder(a);
+        System.out.println();
+        postorder(a);
         System.out.println();
         System.out.println(size(a));
         System.out.println(sum(a));
         System.out.println(product(a));
+        System.out.println(max(a));
+        System.out.println(min(a));
+        System.out.println(levels(a));
+    }
+
+    private static int levels(Node root){
+       if(root == null) return 0;
+       return 1+Math.max(levels(root.left),levels(root.right));
     }
 
     private static int size(Node root){
@@ -54,12 +71,32 @@ public class Implementation {
         int rightsize = product(root.right);
         return root.val * leftsize * rightsize;
     }
+    private static int max(Node root){
+        if(root == null) return Integer.MIN_VALUE;
+        return Math.max(root.val,Math.max(max(root.left),max(root.right)));
+    }
+    private static int min(Node root){
+        if(root == null) return Integer.MAX_VALUE;
+        return Math.min(root.val,Math.min(min(root.left),min(root.right)));
+    }
 
-    public static void display(Node root) {
+    public static void preorder(Node root) {
         if (root == null) return;
         System.out.print(root.val + " ");
-        display(root.left);
-        display(root.right);
+        preorder(root.left);
+        preorder(root.right);
+    }
+    public static void inorder(Node root) {
+        if (root == null) return;
+        inorder(root.left);
+        System.out.print(root.val + " ");
+        inorder(root.right);
+    }
+    public static void postorder(Node root) {
+        if (root == null) return;
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.val + " ");
     }
 }
 
