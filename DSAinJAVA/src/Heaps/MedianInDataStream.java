7905f62897a8062b519bfd8e10f43481c6ab3d7a
RaushanDevloper <raushankumarsingh7033@gmail.com>
2026-07-07 21:59:45 +0530

    add

---
+++ DSAinJAVA/src/Heaps/MedianInDataStream.java
@@ -0,0 +1,50 @@
+package Heaps;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+class MedianFinder {
+
+    PriorityQueue<Integer> minHeap;
+    PriorityQueue<Integer> maxHeap;
+
+    public MedianFinder() {
+        minHeap = new PriorityQueue<>();
+        maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+    }
+
+    public void addNum(int num) {
+        if (maxHeap.size() == 0) {
+            maxHeap.add(num);
+        } else {
+            if (num < maxHeap.peek()) maxHeap.add(num);
+            else minHeap.add(num);
+        }
+
+        if (maxHeap.size() == minHeap.size() + 2) {
+            minHeap.add(maxHeap.remove());
+        }
+
+        if (minHeap.size() == maxHeap.size() + 2) {
+            maxHeap.add(minHeap.remove());
+        }
+    }
+
+    public double findMedian() {
+        if (maxHeap.size() == minHeap.size() + 1) {
+            return maxHeap.peek();
+        }
+        else if (minHeap.size() == maxHeap.size() + 1) {
+            return minHeap.peek();
+        }
+        else {
+            return (maxHeap.peek() + minHeap.peek()) / 2.0;
+        }
+    }
+}
+
+public class MedianInDataStream {
+    public static void main(String[] args) {
+
+    }
+}
