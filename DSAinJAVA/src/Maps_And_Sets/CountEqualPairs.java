a506a2c6787c64ef3fbba0a191a97b15a9fe038f
RaushanDevloper <raushankumarsingh7033@gmail.com>
2026-07-11 15:56:03 +0530

    change

---
+++ DSAinJAVA/src/Maps_And_Sets/CountEqualPairs.java
@@ -0,0 +1,19 @@
+package Maps_And_Sets;
+
+import java.util.HashMap;
+
+public class CountEqualPairs {
+    public int equalPairs(String s) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        long pairs = 0;
+        for(char ch : map.keySet()){
+            long freq = map.get(ch);
+            pairs += freq*freq;
+        }
+        return  (int)pairs;
+    }
+}
---
+++ DSAinJAVA/src/Maps_And_Sets/LongestNonRepeatingSubstring.java
@@ -0,0 +1,26 @@
+package Maps_And_Sets;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class LongestNonRepeatingSubstring {
+    int longestUniqueSubstring(String s) {
+        HashSet<Character> set = new HashSet<>();
+        int i = 0, j = 0, maxLen = 0;
+
+        while (j < s.length()) {
+            char ch = s.charAt(j);
+
+            if (!set.contains(ch)) {
+                set.add(ch);
+                maxLen = Math.max(maxLen, j - i + 1);
+                j++;
+            } else {
+                set.remove(s.charAt(i));
+                i++;
+            }
+        }
+
+        return maxLen;
+    }
+}
