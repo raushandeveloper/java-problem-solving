e0f3767a67305f0a5502f23e8acc06aa2e2bc0e4
RaushanDevloper <raushankumarsingh7033@gmail.com>
2026-07-19 23:35:11 +0530

    main

---
+++ DSAinJAVA/src/BitManipulation/TwoKiPower.java
@@ -0,0 +1,12 @@
+package BitManipulation;
+
+import java.util.Scanner;
+
+public class TwoKiPower {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+
+        System.out.println(1<<n);
+    }
+}
